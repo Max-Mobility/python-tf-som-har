097f72f71d8e4b8ec0ee22a0fa59829318b753f8
William Emfinger <waemfinger@gmail.com>
2018-03-22 16:03:27 -0500

    minor update as we start adding other classifiers and such

---
+++ train_myo_rnn.py
@@ -0,0 +1,45 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import csv
+from som import SOM
+ 
+SOM_X = 20
+SOM_Y = 30
+MYO_DATA_DIM = 3
+SOM_ITER = 100
+
+myo_data = [];
+
+if __name__ == '__main__':
+
+    inputFilename = 'myo_raw_data.csv'
+    header = '% qx, qy, qz, qw, ax, ay, az, gx, gy, gz\n'
+
+    try:
+        f = open(inputFilename, 'r')
+    except Exception as error:
+        print("ERROR: Couldn't read from {}".format(inputFilename))
+    else:
+        with f:
+            reader = csv.reader(f)
+            for row in reader:
+                if row[0][0] is not '%':
+                    myo_data.append(row[4:7])
+            print myo_data[0]
+
+    #Training inputs for Myo data (quat, accel, gyro)
+    som_data = np.array(myo_data)
+
+    #Train a 20x30 SOM with 400 iterations
+    som = SOM(SOM_X, SOM_Y, MYO_DATA_DIM, SOM_ITER)
+    som.train(som_data)
+    
+    #Get output grid
+    image_grid = som.get_centroids()
+
+    print image_grid
+    
+    #Plot
+    plt.imshow(image_grid)
+    plt.title('Myo SOM')
+    plt.show()
